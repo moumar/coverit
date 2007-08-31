b9dc15e5443d81bf12ebac68ecd15f2b1a3abd64
None <none@none>
2007-08-31 16:52:06 +0200

    [svn] [REFACTORED] amazon search include all countries in one query
    --HG--
    branch : trunk

--- search/amazon.rb
+++ search/amazon.rb
@@ -1,42 +1,27 @@
 #amazon search
 require 'amazon/search'
 
-class AmazonBase
-  def search(query)
-    begin
-      res = @req.keyword_search(query, 'music')
-    rescue Amazon::Search::Request::SearchError => e
-      return []
-      #log(e.message + " '#{search_str}'")
-    end
-
-    res.products.collect do |prod|
-      [ prod.image_url_small, prod.image_url_medium, prod.image_url_large ]
-    end.flatten.compact
-  end
-end
-
 module CoverSearch 
-  class Amazon < AmazonBase
-    def initialize
-      @req = ::Amazon::Search::Request.new
-    end
-  end
-
-  class Amazonfr < AmazonBase
-    def initialize
-      @req = ::Amazon::Search::Request.new(nil, nil, 'fr')
-    end
-  end
-
-  class Amazonuk < AmazonBase
-    def initialize
-      @req = ::Amazon::Search::Request.new(nil, nil, 'uk')
+  class Amazon 
+    def search(query)
+      links = []
+      [nil, 'fr', 'uk'].each do |country|
+	begin
+          request = ::Amazon::Search::Request.new(nil, nil, country)
+	  result = request.keyword_search(query, 'music')
+	  result.products.each do |prod|
+	    links << [ prod.image_url_small, prod.image_url_medium, prod.image_url_large ]
+	  end
+	rescue Amazon::Search::Request::SearchError => e
+	  #log(e.message + " '#{search_str}'")
+	end
+      end
+      return links.flatten.compact
     end
   end
 end
 
 if __FILE__ == $0
   require "pp"
-  pp CoverSearch::Amazonfr.new.search("funk fever")
+  pp CoverSearch::Amazon.new.search("funk fever")
 end
